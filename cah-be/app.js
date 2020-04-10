6b56656d0a8d48df9d4cf40d72883957d61f81f1
Nate Ford <ndford2015@gmail.com>
2020-04-10 08:54:26 -0500

    [FIX] routing to be handled by FE

--- cah-be/app.js
+++ cah-be/app.js
@@ -13,8 +13,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'build')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+//app.use('/', indexRouter);
+//app.use('/users', usersRouter);
 
 module.exports = app;
